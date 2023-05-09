6f1b17f14f579ee2c4de16bb5b60b377449f4879
anderson cespedes <andersontrabajos2@gmail.com>
2023-05-09 17:54:32 -0500

    monstruo3

--- generate.js
+++ generate.js
@@ -1,113 +1,128 @@
-class generate{
-    constructor(){
-        this.monsters = {
-            monstruo1:{
-                active:false,
-                x:-600,
-                y:-10,
-                dialog:[
-                "te quiro mucho uwu besame", 
-                "maldito miserable", 
-                "como te atrevez a hablarme asi", 
-                "MUERE!!!!!!!!!"],
-                scene:() => {
-                    if(this.monsters["monstruo1"].x != 40){
-                        this.monsters["monstruo1"].x +=10
-                }
-                else if(this.monsters["monstruo1"].x == 10 && this.monsters["monstruo1"].y == 50){
-                    this.monsters["monstruo1"].x += 2
-                }
-                else{
-                    this.monsters["monstruo1"].active = true
-                }
-                }
-            },
-            monstruo2:{
-                active:false,
-                x:600,
-                y:10,
-                dialog:[
-                "detente animal",
-                "no te lo permitire miserable", 
-                "tienes que ayudarme ",
-                "hay algo raro", 
-                "tienes que ir a la fortaleza de tu madre"],
-                scene:() => {
-                    if(this.monsters["monstruo2"].x >= 100){
-                        this.monsters["monstruo2"].x -=10
-                    }
-                    else if(this.monsters["monstruo2"].x <= 100 && this.monsters["monstruo2"].y >= 50){
-                        this.monsters["monstruo2"].y -= 2
-                    }
-                    else{
-                        this.monsters["monstruo2"].active = true
-                    }
-                }
-                },
-                dialogo:{
-                    active:true,
-                        dialog:[
-                    "Los huevones se fueron a la fortaleza",
-                    "Descubrieron algo increible", 
-                    "La muerte se olia",
-                    "Las calamidades de la muerte", 
-                    "*AAAAAAAHHHHHHHHHHH*"],
-                    scene:() => {
-                        this.monsters["dialogo"].active = true
-                        this.active = true
-                        this.imageBackX +=1 
-                        this.imageBackY +=1 
-                    }
-                    }
-        }
+class generate {
+  constructor() {
+    this.monsters = {
+      monstruo1: {
+        active: false,
+        x: -600,
+        y: -10,
+        dialog: [
+          "te quiro mucho uwu besame",
+          "maldito miserable",
+          "como te atrevez a hablarme asi",
+          "MUERE!!!!!!!!!",
+        ],
+        scene: () => {
+          if (this.monsters["monstruo1"].x != 40) {
+            this.monsters["monstruo1"].x += 10;
+          } else if (
+            this.monsters["monstruo1"].x == 10 &&
+            this.monsters["monstruo1"].y == 50
+          ) {
+            this.monsters["monstruo1"].x += 2;
+          } else {
+            this.monsters["monstruo1"].active = true;
+          }
+        },
+      },
+      monstruo2: {
+        active: false,
+        x: 600,
+        y: 10,
+        dialog: [
+          "detente animal",
+          "no te lo permitire miserable",
+          "tienes que ayudarme ",
+          "hay algo raro",
+          "tienes que ir a la fortaleza de tu madre",
+        ],
+        scene: () => {
+          if (this.monsters["monstruo2"].x >= 100) {
+            this.monsters["monstruo2"].x -= 10;
+          } else if (
+            this.monsters["monstruo2"].x <= 100 &&
+            this.monsters["monstruo2"].y >= 50
+          ) {
+            this.monsters["monstruo2"].y -= 2;
+          } else {
+            this.monsters["monstruo2"].active = true;
+          }
+        },
+      },
+      dialogo: {
+        active: true,
+        dialog: [
+          "Los huevones se fueron a la fortaleza",
+          "Descubrieron algo increible",
+          "La muerte se olia",
+          "Las calamidades de la muerte",
+          "*AAAAAAAHHHHHHHHHHH*",
+        ],
+        scene: () => {
+          this.monsters["dialogo"].active = true;
+          this.active = true;
+          this.imageBackX += 1;
+          this.imageBackY += 1;
+        },
+      },
+      monstruo3: {
+        active: false,
+        x: 50,
+        y: -40,
+        dialog: [],
+        scene: () => {
+          return false;
+        },
+      },
+    };
+  }
+  generateMonster(monster) {
+    this.monsters[monster].scene();
+    if (this.monsters[monster].active == true) {
+      if (this.monsters[monster].dialog.length <= this.dialog) {
+        this.scene += 1;
+        this.textIndex = 0;
+        this.dialog = 0;
+      }
+      let arr = this.monsters[monster].dialog;
+      let texto = arr[this.dialog].split("");
+      if (texto.length > this.textIndex) {
+        this.text += texto[this.textIndex];
+      } else if (texto.length == this.textIndex) {
+        setTimeout(() => {
+          this.text = "";
+          this.dialog += 1;
+          this.textIndex = 0;
+        }, 1);
+      }
+      this.ctx.font = "bold 30px serif";
+      this.ctx.fillStyle = "rgba(0,0,0,0.7)";
+
+      this.ctx.fillRect(0, this.canvas.height - 200, this.canvas.width, 200);
+      this.ctx.fillStyle = "yellow";
+      this.ctx.fillText(this.text, 20, this.canvas.height - this.dialogX);
+      if (this.gameActive == false) this.textIndex += 1;
     }
-    generateMonster(monster){
-        this.monsters[monster].scene()
-        if(this.monsters[monster].active == true){
-            if(this.monsters[monster].dialog.length <= this.dialog){
-                this.scene +=1
-                this.textIndex = 0;
-                this.dialog = 0;
-            }
-            let arr = this.monsters[monster].dialog
-            let texto = arr[this.dialog].split("");
-            if(texto.length > this.textIndex){
-                this.text += texto[this.textIndex]
-            }
-            else if (texto.length == this.textIndex){
-                setTimeout(() => {
-                    this.text = ""
-                    this.dialog += 1;
-                    this.textIndex = 0;  
-                }, 1)
-            }
-            this.ctx.font = "bold 30px serif";
-            this.ctx.fillStyle = "rgba(0,0,0,0.7)";
-            
-            this.ctx.fillRect(0,this.canvas.height - 200,this.canvas.width,200)
-            this.ctx.fillStyle = "yellow";
-            this.ctx.fillText(this.text ,20,this.canvas.height - this.dialogX );
-            if(this.gameActive == false) this.textIndex +=1;
-        }
-    
-}
-    drawMonster(param, monster){
+  }
+  drawMonster(param, monster) {
     let image = new Image();
     let count = 0;
-    if(monster != "dialogo"){
-        image.onload = () => {
-            image.height;
-            image.width;
-            this.ctx.drawImage(image, this.monsters[monster].x, this.monsters[monster].y,image.width,image.height);
-            count++
-            this.generateMonster(monster)
-            }
-         image.src = param  
-     }   
-     else{
-        this.generateMonster(monster)
-     
-     }
-     
+    if (monster != "dialogo") {
+      image.onload = () => {
+        image.height;
+        image.width;
+        this.ctx.drawImage(
+          image,
+          this.monsters[monster].x,
+          this.monsters[monster].y,
+          image.width < 400 ? image.width : 120,
+          image.height < 400 ? image.height : 120
+        );
+        count++;
+        this.generateMonster(monster);
+      };
+      image.src = param;
+    } else {
+      this.generateMonster(monster);
     }
+  }
 }
--- index.js
+++ index.js
@@ -1,128 +1,155 @@
-class Over extends generate{
-    constructor(params, obj){
-        super()
-        this.personaje = new Character;
-        this.statsPersonaje = this.personaje.stdist;
-        this.activeBlade = false;
-        this.bladeX  = 30;
-        this.bladeY = 40;
-        this.gameActive = false;
-        this.canvas = params.canvas;
-        this.ctx = params.ctx;
-        this.obj = obj;
-        this.imageBackX = 0;
-        this.imageBackY = 0;
-        this.scene = 0;
-        this.textIndex = 0;
-        this.text = ""
-        this.dialog = 0;
-        this.dialogX = 100;
-        this.currentFrame = 4;
+class Over extends generate {
+  constructor(params, obj) {
+    super();
+    this.personaje = new Character();
+    this.statsPersonaje = this.personaje.stdist;
+    this.activeBlade = false;
+    this.bladeX = 30;
+    this.bladeY = 40;
+    this.gameActive = false;
+    this.canvas = params.canvas;
+    this.ctx = params.ctx;
+    this.obj = obj;
+    this.imageBackX = 0;
+    this.imageBackY = 0;
+    this.scene = 0;
+    this.textIndex = 0;
+    this.text = "";
+    this.dialog = 0;
+    this.dialogX = 100;
+    this.color = "blue";
+    this.currentFrame = 4;
+  }
+  atackAnimation() {
+    if (this.activeBlade == true) {
+      let image = new Image();
+      var frameWidth = 170 - 48.9; // ancho de cada fotograma en píxeles
+      var frameHeight = 530; // alto de cada fotograma en píxeles
+      var totalFrames = 9; // número total de fotogramas en la animación
+      var animationSpeed = 100; //
+      if (this.currentFrame >= totalFrames) {
+        this.currentFrame = 0; // Reiniciar la animación al llegar al último fotograma
+      }
+      // Dibujar el fotograma actual
+      var frameX = this.currentFrame * frameWidth; // posición x del fotograma en la imagen del sprite
+      image.onload = () => {
+        this.ctx.drawImage(
+          image,
+          frameX,
+          590,
+          frameWidth,
+          frameHeight - 30,
+          90,
+          -30,
+          frameWidth - 70,
+          frameHeight
+        );
+      };
+      image.src = "a1.png";
+      this.currentFrame += 1;
     }
-     atackAnimation(){
-        if(this.activeBlade == true){
-            let image = new Image();
-            var frameWidth = 170- 48.9; // ancho de cada fotograma en píxeles
-            var frameHeight = 530; // alto de cada fotograma en píxeles
-            var totalFrames = 9; // número total de fotogramas en la animación
-            var animationSpeed = 100; // 
-            if (this.currentFrame >= totalFrames) {
-                this.currentFrame = 0; // Reiniciar la animación al llegar al último fotograma
-              }
-              // Dibujar el fotograma actual
-              var frameX = this.currentFrame * frameWidth; // posición x del fotograma en la imagen del sprite
-            image.onload =() => {
-                this.ctx.drawImage(image, frameX, 590, frameWidth , frameHeight - 30, 90, -30, frameWidth - 70, frameHeight);
-            }
-            image.src = "a1.png" 
-            this.currentFrame+=1;
-        }
-    }
+  }
 
-    drawButton(){
-        if(this.gameActive == true){
-                this.ctx.fillStyle = "blue";
-                this.ctx.fillRect(40, this.canvas.height - 50 - 20, 100, 30);
-                this.ctx.fillStyle = "red";
-                this.ctx.strokeRect(40, this.canvas.height - 50 - 20, 100, 30);
-                this.ctx.font = "20px Arial";
-                this.ctx.fillStyle = "white";
-                this.ctx.textAlign = "center";
-                this.ctx.fillText("Ataque", 83, canvas.height - 45);
-                this.ctx.fillRect(130, this.canvas.height - 50 - 20, 170, 80);
-                this.ctx.strokeRect(130, this.canvas.height - 50 - 20, 170, 80);
-                
-                this.ctx.fillStyle = "black";
-                this.ctx.font = "15px Arial";
-                this.ctx.fillText(`HP:50/${this.statsPersonaje.vit}`, 175, canvas.height - 45);
-                this.ctx.fillText(`PP:20/${this.statsPersonaje.pp}`, 175, canvas.height - 20);
-            }
+  drawButton() {
+    if (this.gameActive == true) {
+      this.ctx.fillStyle = this.color;
+      this.ctx.fillRect(40, this.canvas.height - 50 - 20, 100, 30);
+      this.ctx.fillStyle = "red";
+      this.ctx.strokeRect(40, this.canvas.height - 50 - 20, 100, 30);
+      this.ctx.font = "20px Arial";
+      this.ctx.fillStyle = "white";
+      this.ctx.textAlign = "center";
+      this.ctx.fillText("Ataque", 83, canvas.height - 45);
+      this.ctx.fillRect(130, this.canvas.height - 50 - 20, 170, 80);
+      this.ctx.strokeRect(130, this.canvas.height - 50 - 20, 170, 80);
+
+      this.ctx.fillStyle = "black";
+      this.ctx.font = "15px Arial";
+      this.ctx.fillText(
+        `HP:50/${this.statsPersonaje.vit}`,
+        175,
+        canvas.height - 45
+      );
+      this.ctx.fillText(
+        `PP:20/${this.statsPersonaje.pp}`,
+        175,
+        canvas.height - 20
+      );
     }
-    draw(){
+  }
+  draw() {
     const a = () => {
-        let image = new Image();
-        this.ctx.clearRect( 0, 0,image.width,image.height);
-                image.onload = () => {
-                    
-                    this.ctx.canvas.width = image.width;
-                    this.ctx.canvas.height = image.height;
-                    this.ctx.drawImage(image, 0, 0,image.width + this.imageBackX,image.height + this.imageBackY);
-                    if(this.scene == 0){
-                        this.drawMonster("Real-Monster-PNG-Clipart.png", "monstruo1" )
-                    }
-                    else if(this.scene == 1){
-                        this.drawMonster("pngegg.png", "monstruo2")
-                    }
-                    else if (this.scene == 2){
-                        this.drawMonster("", "dialogo");
-                    }
-                    else if (this.scene == 3){
-                        this.drawButton()
-                        this.obj.src = "ruins2.png"
-                        this.gameActive = true;
-                        this.imageBackX =0
-                        this.imageBackY =0
-                        this.atackAnimation()
-                    }
-                }
-            requestAnimationFrame(a)
-            image.src = this.obj.src;
+      let image = new Image();
+      this.ctx.clearRect(0, 0, image.width, image.height);
+      image.onload = () => {
+        this.ctx.canvas.width = image.width;
+        this.ctx.canvas.height = image.height;
+        this.ctx.drawImage(
+          image,
+          0,
+          0,
+          image.width + this.imageBackX,
+          image.height + this.imageBackY
+        );
+        if (this.scene == 0) {
+          this.drawMonster("Real-Monster-PNG-Clipart.png", "monstruo1");
+        } else if (this.scene == 1) {
+          this.drawMonster("pngegg.png", "monstruo2");
+        } else if (this.scene == 2) {
+          this.drawMonster("", "dialogo");
+        } else if (this.scene == 3) {
+          this.drawMonster("monster_amiba.png", "monstruo3");
+          this.drawButton();
+          this.obj.src = "ruins2.png";
+          this.gameActive = true;
+          this.imageBackX = 0;
+          this.imageBackY = 0;
+          this.atackAnimation();
         }
-        requestAnimationFrame(a)
-    }
-    actions(){
-        this.canvas.addEventListener("mousedown", (event) =>{
-                var rect = this.canvas.getBoundingClientRect();
-                var x = event.clientX - rect.left;
-                var y = event.clientY - rect.top;
-                if (x > 100 && x < 350 && y > 360 && y < 450 ) {
-                    this.activeBlade = true;
-                    this.personaje.dagno = 3;
-                    setTimeout(() => {
-                        this.activeBlade = false;
-                        this.currentFrame = 0;
-                        this.bladeX = 30;
-                     }, 200 )
-                }
-        });
-        this.canvas.addEventListener("mousemove", (event) => {
-            var rect = this.canvas.getBoundingClientRect();
-            var x = event.clientX - rect.left;
-            var y = event.clientY - rect.top;
-            if (x > 100 && x < 330 && y > 360 && y < 450 ) {
-                this.ctx.fillStyle = "green";
-                this.canvas.style.cursor = "pointer";
-            }
-            else{
-                this.canvas.style.cursor = "default";
-            }
-        })
-    }
-    init(){
-        this.actions()
-        this.draw()
-    }
+      };
+      requestAnimationFrame(a);
+      image.src = this.obj.src;
+    };
+    requestAnimationFrame(a);
+  }
+  actions() {
+    this.canvas.addEventListener("mousedown", (event) => {
+      var rect = this.canvas.getBoundingClientRect();
+      var x = event.clientX - rect.left;
+      var y = event.clientY - rect.top;
+      if (x > 100 && x < 350 && y > 360 && y < 450) {
+        this.activeBlade = true;
+
+        setTimeout(() => {
+          this.activeBlade = false;
+          this.currentFrame = 0;
+          this.bladeX = 30;
+          this.personaje.dagno = 3;
+        }, 300);
+      }
+    });
+    this.canvas.addEventListener("mousemove", (event) => {
+      var rect = this.canvas.getBoundingClientRect();
+      var x = event.clientX - rect.left;
+      var y = event.clientY - rect.top;
+      if (x > 100 && x < 330 && y > 360 && y < 450) {
+        this.color = "red";
+        this.ctx.fillStyle = "green";
+        this.canvas.style.cursor = "pointer";
+      } else {
+        this.color = "blue";
+        this.canvas.style.cursor = "default";
+      }
+    });
+  }
+  init() {
+    this.actions();
+    this.draw();
+  }
 }
-let canvas = document.getElementById("canvas")
-const over = new Over({canvas:canvas, ctx:canvas.getContext("2d")}, {src:"shutterstock_96456725.jpg"})
-over.init()
+let canvas = document.getElementById("canvas");
+const over = new Over(
+  { canvas: canvas, ctx: canvas.getContext("2d") },
+  { src: "shutterstock_96456725.jpg" }
+);
+over.init();
